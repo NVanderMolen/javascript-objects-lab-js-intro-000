28913833d64940e0161d90612a98a204a3ea787e
Nick VanderMolen <tree.vandermolen@gmail.com>
2020-04-01 12:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 function deleteFromObjectByKey(object, key){
-  var newObj = object.assign ({}, object);
+  var newObj = object.assign ({}, {[key]});
   delete newObj[key];
   return newObj;
 
